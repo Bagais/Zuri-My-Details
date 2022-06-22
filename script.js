fcde7086d94e43bbe8a1071021d4b0c7d1975cf3
Elisha Bagaiya <105201298+Bagais@users.noreply.github.com>
2022-06-22 10:16:06 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+let name = 'Elisha Bagaiya';
+let height = '165cm'
+let country = 'Nigeria'
+
+console.log('Name = ' + name)
+console.log('Height = ' + height)
+console.log('Country = ' + country)
+
+alert('My name is ' + name)
+alert('I am ' + height + ' tall')
+alert('and proudly ' + country + 'n')
