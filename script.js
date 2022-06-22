1d3e6cb8bd033910ae06aef0547e52e80f158b22
Elisha Bagaiya <105201298+Bagais@users.noreply.github.com>
2022-06-22 10:38:13 +0100

    second commit

--- script.js
+++ script.js
@@ -6,6 +6,4 @@ console.log('Name = ' + name)
 console.log('Height = ' + height)
 console.log('Country = ' + country)
 
-alert('My name is ' + name)
-alert('I am ' + height + ' tall')
-alert('and proudly ' + country + 'n')
+alert('My name is ' + name +'.' + '\nI am ' + height + ' tall,' + '\nand proudly ' + country + 'n!')
